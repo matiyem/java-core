92be9019f76f81fbf933117013dd2c9255f59533
alai <alai@behsazan.net>
2022-01-01 09:03:44 +0330

    committed

---
+++ java-core-streams-3/src/main/java/com/example/closure/StreamClosureSnippets.java
@@ -0,0 +1,17 @@
+package com.example.closure;
+
+import java.util.Arrays;
+
+/**
+ * created by Atiye Mousavi
+ * Date: 12/31/2021
+ * Time: 6:35 PM
+ **/
+
+
+public class StreamClosureSnippets {
+
+    public static void main(String[] args) {
+        Arrays.asList("Red","Blue","Green")
+    }
+}
